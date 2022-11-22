215d8178cde0d444b810cb894776c8c5a17340e9
William Lee <wlee753159@gmail.com>
2022-11-22 02:07:04 +0000

    Fix copy_data.sh (this time)

--- hpc/copy_data.sh
+++ hpc/copy_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Make /fastdata directory if it doesn't exist
-FASTDATA_DIR="/fastdata/$(whoami)"
+FASTDATA_DIR="/fastdata/$(whoami)/distilbert/dataset"
 if [ ! -d "$FASTDATA_DIR" ]; then
   # Make personal fastdata directory
-  mkdir "$FASTDATA_DIR/distilbert/dataset"
+  mkdir "$FASTDATA_DIR"
   # Make it private to your user
   chmod 700 "$FASTDATA_DIR"
 fi
 
-cp -R distilbert/dataset "$FASTDATA_DIR/distilbert/dataset"
+cp -R distilbert/dataset "$FASTDATA_DIR"
