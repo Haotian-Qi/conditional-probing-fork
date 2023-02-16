2c92342043bdc34413b83478f3558d0bfaa1f186
William Lee <wlee753159@gmail.com>
2023-02-16 16:04:12 +0000

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 DATA_DIR="$(dirname $0)/data"
+ROOT_URL="https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/"
+FILENAMES=( 
+    "dev.ontonotes.withdep.conll"
+    "test.ontonotes.withdep.conll"
+    "train.ontonotes.withdep.conll"
+    "sst2-dev.tsv"
+    "sst2-test.tsv"
+    "sst2-train.tsv"
+    "dev.ontonotes.withdep.conll.cache.hfacetokensrobertabase.hdf5"
+    "test.ontonotes.withdep.conll.cache.hfacetokensrobertabase.hdf5"
+    "train.ontonotes.withdep.conll.cache.hfacetokensrobertabase.hdf5"
+    "sst2-dev.tsv.cache.hfacetokensrobertabase.hdf5"
+    "sst2-test.tsv.cache.hfacetokensrobertabase.hdf5"
+    "sst2-train.tsv.cache.hfacetokensrobertabase.hdf5"
+)
+
 mkdir -p $DATA_DIR
 cd $DATA_DIR
 
-declare -A data_urls=( 
-    [dev.ontonotes.withdep.conll]=https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/dev.ontonotes.withdep.conll
-    [test.ontonotes.withdep.conll]=https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/test.ontonotes.withdep.conll
-    [train.ontonotes.withdep.conll]=https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/train.ontonotes.withdep.conll
-    [sst2-dev.tsv]=https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/sst2-dev.tsv
-    [sst2-test.tsv]=https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/sst2-test.tsv
-    [sst2-train.tsv]=https://worksheets.codalab.org/rest/bundles/0x6f3556ec9edf4774a2db0ad88f140fac/contents/blob/sst2-train.tsv
-)
-
-for filename in ${!data_urls[@]}; do
-    url=${data_urls[$filename]}
-    curl "$url" -o "$filename"
+for i in ${!FILENAMES[@]}; do
+    filename=${FILENAMES[i]}
+    url="${ROOT_URL}${filename}"
+    echo "${url}"
+    curl "${url}" -o "${filename}"
 done
